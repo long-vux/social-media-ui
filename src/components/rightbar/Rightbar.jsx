612a9524d7cff39785932d46b1a8ccb59f14b70d
Long Vu <hoanglongvu233@gmail.com>
2024-06-27 20:42:42 +0700

    Completed profile page ui

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,10 +2,10 @@ import Online from "../online/Online";
 import "./rightbar.css";
 import { Users } from "../../dummyData";
 
-export default function Rightbar() {
-  return (
-    <div className="rightbar">
-      <div className="rightbarWrapper">
+export default function Rightbar({ profile }) {
+  const HomeRightbar = () => {
+    return (
+      <>
         <div className="birthdayContainer">
           <img className="birthdayImg" src="assets/gift.png" alt="" />
           <span className="birthdayText">
@@ -19,6 +19,62 @@ export default function Rightbar() {
             <Online key={u.id} user={u} />
           ))}
         </ul>
+      </>
+    );
+  };
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">User information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue"> Ho Chi Minh</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue"> Dong Thap - Viet Nam</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Relationship:</span>
+            <span className="rightbarInfoValue"> Dating</span>
+          </div>
+        </div>
+        <div className="rightbarTitle">User friends</div>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img
+              className="rightbarFollowingImg"
+              src="assets/person/1.jpeg"
+              alt=""
+            />
+            <span className="rightbarFollowingName">Long Vu</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              className="rightbarFollowingImg"
+              src="assets/person/1.jpeg"
+              alt=""
+            />
+            <span className="rightbarFollowingName">Long Vu</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              className="rightbarFollowingImg"
+              src="assets/person/1.jpeg"
+              alt=""
+            />
+            <span className="rightbarFollowingName">Long Vu</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <ProfileRightbar />
       </div>
     </div>
   );
